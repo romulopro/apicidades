df3bf6cf28d5b683765f298da6f9365b15774359
romulooc <romuloprosp@gmail.com>
2021-11-05 22:24:39 -0300

    first commit

---
+++ src/main/java/one/dio/cidades/api/apicidades/ApicidadesApplication.java
@@ -0,0 +1,13 @@
+package one.dio.cidades.api.apicidades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApicidadesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApicidadesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/one/dio/cidades/api/apicidades/ApicidadesApplicationTests.java
@@ -0,0 +1,13 @@
+package one.dio.cidades.api.apicidades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApicidadesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
